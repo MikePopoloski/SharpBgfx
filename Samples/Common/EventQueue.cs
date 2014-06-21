ddc6159cb75c4503aec88c84190defe2b5aedd69
Mike Popoloski <mike@popoloski.com>
2014-06-21 10:12:24 -0700

    Adding Common project, organizing the solution a bit, making the DrawStress sample use more of the library.

---
+++ Samples/Common/EventQueue.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Concurrent;
+
+namespace Common {
+    public class EventQueue {
+        ConcurrentQueue<Event> queue = new ConcurrentQueue<Event>();
+
+        public void Post (Event ev) {
+            queue.Enqueue(ev);
+        }
+
+        public Event Poll () {
+            Event ev;
+            if (queue.TryDequeue(out ev))
+                return ev;
+
+            return null;
+        }
+    }
+
+    public enum EventType {
+        Exit,
+        Key,
+        Mouse,
+        Size
+    }
+
+    public class Event {
+        public EventType Type {
+            get;
+            private set;
+        }
+
+        public Event (EventType type) {
+            Type = type;
+        }
+    }
+
+    public class SizeEvent : Event {
+        public int Width {
+            get;
+            private set;
+        }
+
+        public int Height {
+            get;
+            private set;
+        }
+
+        public SizeEvent (int width, int height)
+            : base(EventType.Size) {
+
+            Width = width;
+            Height = height;
+        }
+    }
+}
--- Samples/DrawStress/Program.cs
+++ Samples/DrawStress/Program.cs
@@ -2,31 +2,81 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Common;
 using SharpBgfx;
 
 namespace DrawStress {
-    unsafe static class Program {
+    static class Program {
+        static EventQueue eventQueue = new EventQueue();
+        static int windowWidth = 1280;
+        static int windowHeight = 720;
+
         static void Main () {
             var form = new Form {
                 Text = "Draw Stress Test",
-                ClientSize = new Size(920, 760)
+                ClientSize = new Size(windowWidth, windowHeight)
             };
 
+            form.ClientSizeChanged += (o, e) => eventQueue.Post(new SizeEvent(form.ClientSize.Width, form.ClientSize.Height));
+            form.FormClosed += (o, e) => eventQueue.Post(new Event(EventType.Exit));
+
             Bgfx.SetWindowHandle(form.Handle);
-            Bgfx.Init(RendererType.OpenGL, IntPtr.Zero, IntPtr.Zero);
 
-            var renderers = Bgfx.GetSupportedRenderers();
+            eventQueue.Post(new SizeEvent(form.ClientSize.Width, form.ClientSize.Height));
 
-            var current = Bgfx.GetCurrentRenderer();
-            var name = Bgfx.GetRendererName(current);
-
-            var caps = Bgfx.GetCaps();
+            var thread = new Thread(RenderThread);
+            thread.Start();
 
             Application.Run(form);
 
+            thread.Join();
+        }
+
+        static void MainLoop () {
+            Bgfx.SetViewRect(0, 0, 0, (ushort)windowWidth, (ushort)windowHeight);
+            Bgfx.Submit(0, 0);
+
+            Bgfx.Frame();
+        }
+
+        static void RenderThread () {
+            Bgfx.Init(RendererType.OpenGL, IntPtr.Zero, IntPtr.Zero);
+            Bgfx.Reset(windowWidth, windowHeight, ResetFlags.None);
+            Bgfx.SetDebugFlags(DebugFlags.DisplayText);
+
+            Bgfx.SetViewClear(0, ClearFlags.ColorBit | ClearFlags.DepthBit, 0x303030ff, 1.0f, 0);
+
+            while (ProcessEvents())
+                MainLoop();
+
             Bgfx.Shutdown();
+        }
+
+        static bool ProcessEvents () {
+            Event ev;
+            bool resizeRequired = false;
+
+            while ((ev = eventQueue.Poll()) != null) {
+                switch (ev.Type) {
+                    case EventType.Exit:
+                        return false;
+
+                    case EventType.Size:
+                        var size = (SizeEvent)ev;
+                        windowWidth = size.Width;
+                        windowHeight = size.Height;
+                        resizeRequired = true;
+                        break;
+                }
+            }
+
+            if (resizeRequired)
+                Bgfx.Reset(windowWidth, windowHeight, ResetFlags.None);
+
+            return true;
         }
     }
 }
