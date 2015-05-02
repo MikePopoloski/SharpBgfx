08cd824ec09a570762410570e2c0b687b2b2d533
Mike Popoloski <mike@popoloski.com>
2015-05-02 12:16:26 -0400

    Fixing line endings.

--- SharpBgfx/TransientIndexBuffer.cs
+++ SharpBgfx/TransientIndexBuffer.cs
@@ -1,36 +1,36 @@
 ﻿using System;
 
 namespace SharpBgfx {
-	/// <summary>
-	/// Maintains a transient index buffer.
-	/// </summary>
-	/// <remarks>
-	/// The contents of the buffer are valid for the current frame only.
-	/// You must call SetVertexBuffer with the buffer or a leak could occur.
-	/// </remarks>
-	public unsafe struct TransientIndexBuffer : IEquatable<TransientIndexBuffer> {
+    /// <summary>
+    /// Maintains a transient index buffer.
+    /// </summary>
+    /// <remarks>
+    /// The contents of the buffer are valid for the current frame only.
+    /// You must call SetVertexBuffer with the buffer or a leak could occur.
+    /// </remarks>
+    public unsafe struct TransientIndexBuffer : IEquatable<TransientIndexBuffer> {
         readonly IntPtr data;
-		int size;
-		int startIndex;
+        int size;
+        int startIndex;
         readonly ushort handle;
 
-		/// <summary>
-		/// A pointer that can be filled with index data.
-		/// </summary>
-		public IntPtr Data => data;
+        /// <summary>
+        /// A pointer that can be filled with index data.
+        /// </summary>
+        public IntPtr Data => data;
 
         /// <summary>
         /// The size of the buffer.
         /// </summary>
         public int Count => size;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="TransientIndexBuffer"/> struct.
-		/// </summary>
-		/// <param name="indexCount">The number of 16-bit indices that fit in the buffer.</param>
-		public TransientIndexBuffer (int indexCount) {
-			NativeMethods.bgfx_alloc_transient_index_buffer(out this, indexCount);
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransientIndexBuffer"/> struct.
+        /// </summary>
+        /// <param name="indexCount">The number of 16-bit indices that fit in the buffer.</param>
+        public TransientIndexBuffer (int indexCount) {
+            NativeMethods.bgfx_alloc_transient_index_buffer(out this, indexCount);
+        }
 
         /// <summary>
         /// Check if there is available space in the global transient index buffer.
@@ -41,39 +41,39 @@ namespace SharpBgfx {
             return NativeMethods.bgfx_check_avail_transient_index_buffer(count);
         }
 
-		/// <summary>
-		/// Determines whether the specified object is equal to this instance.
-		/// </summary>
-		/// <param name="other">The object to compare with this instance.</param>
-		/// <returns><c>true</c> if the specified object is equal to this instance; otherwise, <c>false</c>.</returns>
-		public bool Equals (TransientIndexBuffer other) {
-			return handle == other.handle && data == other.data;
-		}
+        /// <summary>
+        /// Determines whether the specified object is equal to this instance.
+        /// </summary>
+        /// <param name="other">The object to compare with this instance.</param>
+        /// <returns><c>true</c> if the specified object is equal to this instance; otherwise, <c>false</c>.</returns>
+        public bool Equals (TransientIndexBuffer other) {
+            return handle == other.handle && data == other.data;
+        }
 
-		/// <summary>
-		/// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
-		/// </summary>
-		/// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
-		/// <returns>
-		///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
-		/// </returns>
-		public override bool Equals (object obj) {
-			var other = obj as TransientIndexBuffer?;
-			if (other == null)
-				return false;
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals (object obj) {
+            var other = obj as TransientIndexBuffer?;
+            if (other == null)
+                return false;
 
-			return Equals(other);
-		}
+            return Equals(other);
+        }
 
-		/// <summary>
-		/// Returns a hash code for this instance.
-		/// </summary>
-		/// <returns>
-		/// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
-		/// </returns>
-		public override int GetHashCode () {
-			return handle.GetHashCode() >> 13 ^ data.GetHashCode();
-		}
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode () {
+            return handle.GetHashCode() >> 13 ^ data.GetHashCode();
+        }
 
         /// <summary>
         /// Returns a <see cref="System.String"/> that represents this instance.
@@ -92,19 +92,19 @@ namespace SharpBgfx {
         /// <c>true</c> if the two objects are equal; otherwise, <c>false</c>.
         /// </returns>
         public static bool operator ==(TransientIndexBuffer left, TransientIndexBuffer right) {
-			return left.Equals(right);
-		}
+            return left.Equals(right);
+        }
 
-		/// <summary>
-		/// Implements the inequality operator.
-		/// </summary>
-		/// <param name="left">The left side of the operator.</param>
-		/// <param name="right">The right side of the operator.</param>
-		/// <returns>
-		/// <c>true</c> if the two objects are not equal; otherwise, <c>false</c>.
-		/// </returns>
-		public static bool operator !=(TransientIndexBuffer left, TransientIndexBuffer right) {
-			return !left.Equals(right);
-		}
-	}
+        /// <summary>
+        /// Implements the inequality operator.
+        /// </summary>
+        /// <param name="left">The left side of the operator.</param>
+        /// <param name="right">The right side of the operator.</param>
+        /// <returns>
+        /// <c>true</c> if the two objects are not equal; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool operator !=(TransientIndexBuffer left, TransientIndexBuffer right) {
+            return !left.Equals(right);
+        }
+    }
 }
--- SharpBgfx/TransientVertexBuffer.cs
+++ SharpBgfx/TransientVertexBuffer.cs
@@ -1,85 +1,85 @@
 ﻿using System;
 
 namespace SharpBgfx {
-	/// <summary>
-	/// Maintains a transient vertex buffer.
-	/// </summary>
-	/// <remarks>
-	/// The contents of the buffer are valid for the current frame only.
-	/// You must call SetVertexBuffer with the buffer or a leak could occur.
-	/// </remarks>
-	public unsafe struct TransientVertexBuffer : IEquatable<TransientVertexBuffer> {
+    /// <summary>
+    /// Maintains a transient vertex buffer.
+    /// </summary>
+    /// <remarks>
+    /// The contents of the buffer are valid for the current frame only.
+    /// You must call SetVertexBuffer with the buffer or a leak could occur.
+    /// </remarks>
+    public unsafe struct TransientVertexBuffer : IEquatable<TransientVertexBuffer> {
         readonly IntPtr data;
-		int size;
-		int startVertex;
-		ushort stride;
+        int size;
+        int startVertex;
+        ushort stride;
         readonly ushort handle;
-		ushort decl;
+        ushort decl;
 
-		/// <summary>
-		/// A pointer that can be filled with vertex data.
-		/// </summary>
-		public IntPtr Data => data;
+        /// <summary>
+        /// A pointer that can be filled with vertex data.
+        /// </summary>
+        public IntPtr Data => data;
 
-		/// <summary>
-		/// The size of the buffer.
-		/// </summary>
-		public int Count => size;
+        /// <summary>
+        /// The size of the buffer.
+        /// </summary>
+        public int Count => size;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="TransientVertexBuffer"/> struct.
-		/// </summary>
-		/// <param name="vertexCount">The number of vertices that fit in the buffer.</param>
-		/// <param name="layout">The layout of the vertex data.</param>
-		public TransientVertexBuffer (int vertexCount, VertexLayout layout) {
-			NativeMethods.bgfx_alloc_transient_vertex_buffer(out this, vertexCount, ref layout.data);
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransientVertexBuffer"/> struct.
+        /// </summary>
+        /// <param name="vertexCount">The number of vertices that fit in the buffer.</param>
+        /// <param name="layout">The layout of the vertex data.</param>
+        public TransientVertexBuffer (int vertexCount, VertexLayout layout) {
+            NativeMethods.bgfx_alloc_transient_vertex_buffer(out this, vertexCount, ref layout.data);
+        }
 
-		/// <summary>
-		/// Check if there is available space in the global transient vertex buffer.
-		/// </summary>
-		/// <param name="count">The number of vertices to allocate.</param>
-		/// <param name="layout">The layout of each vertex.</param>
-		/// <returns>
-		///   <c>true</c> if there is sufficient space for the give number of vertices.
-		/// </returns>
-		public static bool CheckAvailableSpace (int count, VertexLayout layout) {
-			return NativeMethods.bgfx_check_avail_transient_vertex_buffer(count, ref layout.data);
-		}
+        /// <summary>
+        /// Check if there is available space in the global transient vertex buffer.
+        /// </summary>
+        /// <param name="count">The number of vertices to allocate.</param>
+        /// <param name="layout">The layout of each vertex.</param>
+        /// <returns>
+        ///   <c>true</c> if there is sufficient space for the give number of vertices.
+        /// </returns>
+        public static bool CheckAvailableSpace (int count, VertexLayout layout) {
+            return NativeMethods.bgfx_check_avail_transient_vertex_buffer(count, ref layout.data);
+        }
 
-		/// <summary>
-		/// Determines whether the specified object is equal to this instance.
-		/// </summary>
-		/// <param name="other">The object to compare with this instance.</param>
-		/// <returns><c>true</c> if the specified object is equal to this instance; otherwise, <c>false</c>.</returns>
-		public bool Equals (TransientVertexBuffer other) {
-			return handle == other.handle && data == other.data;
-		}
+        /// <summary>
+        /// Determines whether the specified object is equal to this instance.
+        /// </summary>
+        /// <param name="other">The object to compare with this instance.</param>
+        /// <returns><c>true</c> if the specified object is equal to this instance; otherwise, <c>false</c>.</returns>
+        public bool Equals (TransientVertexBuffer other) {
+            return handle == other.handle && data == other.data;
+        }
 
-		/// <summary>
-		/// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
-		/// </summary>
-		/// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
-		/// <returns>
-		///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
-		/// </returns>
-		public override bool Equals (object obj) {
-			var other = obj as TransientVertexBuffer?;
-			if (other == null)
-				return false;
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals (object obj) {
+            var other = obj as TransientVertexBuffer?;
+            if (other == null)
+                return false;
 
-			return Equals(other);
-		}
+            return Equals(other);
+        }
 
-		/// <summary>
-		/// Returns a hash code for this instance.
-		/// </summary>
-		/// <returns>
-		/// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
-		/// </returns>
-		public override int GetHashCode () {
-			return handle.GetHashCode() >> 13 ^ data.GetHashCode();
-		}
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode () {
+            return handle.GetHashCode() >> 13 ^ data.GetHashCode();
+        }
 
         /// <summary>
         /// Returns a <see cref="System.String"/> that represents this instance.
@@ -98,19 +98,19 @@ namespace SharpBgfx {
         /// <c>true</c> if the two objects are equal; otherwise, <c>false</c>.
         /// </returns>
         public static bool operator ==(TransientVertexBuffer left, TransientVertexBuffer right) {
-			return left.Equals(right);
-		}
+            return left.Equals(right);
+        }
 
-		/// <summary>
-		/// Implements the inequality operator.
-		/// </summary>
-		/// <param name="left">The left side of the operator.</param>
-		/// <param name="right">The right side of the operator.</param>
-		/// <returns>
-		/// <c>true</c> if the two objects are not equal; otherwise, <c>false</c>.
-		/// </returns>
-		public static bool operator !=(TransientVertexBuffer left, TransientVertexBuffer right) {
-			return !left.Equals(right);
-		}
-	}
+        /// <summary>
+        /// Implements the inequality operator.
+        /// </summary>
+        /// <param name="left">The left side of the operator.</param>
+        /// <param name="right">The right side of the operator.</param>
+        /// <returns>
+        /// <c>true</c> if the two objects are not equal; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool operator !=(TransientVertexBuffer left, TransientVertexBuffer right) {
+            return !left.Equals(right);
+        }
+    }
 }
