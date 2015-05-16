7c375dcffdb88cf09bc396292c41d7533daf36f1
Mike Popoloski <mike@popoloski.com>
2015-05-16 15:11:12 -0400

    Adding compute shaders for NBody sample.

--- examples/Assets/Shaders/compile.ps1
+++ examples/Assets/Shaders/compile.ps1
@@ -1,12 +1,12 @@
 $s = 0
 $e = 0
 
-Function Compile ($filter, $type, $profile) {
+Function Compile ($filter, $type, $profile, $glProfile) {
 	foreach ($file in (ls -Path $dir -Filter $filter)) {
 		$path = $file | Resolve-Path -Relative
         $outname = [io.path]::ChangeExtension($file.Name, "bin")
 		Write-Output ("Compiling {0}..." -f $path)
-		&..\..\..\Tools\shaderc.exe --platform linux -p 120 --type $type -f "$path" -o ".\bin\glsl\$outname" -i ..\
+		&..\..\..\Tools\shaderc.exe --platform linux -p $glProfile --type $type -f "$path" -o ".\bin\glsl\$outname" -i ..\
 		if ($LastExitCode -eq 0) { $Global:s++ } Else { $Global:e++ }
         &..\..\..\Tools\shaderc.exe --platform windows -p $profile -O 3 --type $type -f "$path" -o ".\bin\dx11\$outname" -i ..\
 		if ($LastExitCode -eq 0) { $Global:s++ } Else { $Global:e++ }
@@ -14,8 +14,9 @@ Function Compile ($filter, $type, $profile) {
 }
 
 foreach ($dir in (ls -Directory)) {
-	Compile "vs_*.sc" "vertex" "vs_4_0"
-	Compile "fs_*.sc" "fragment" "ps_4_0"
+	Compile "vs_*.sc" "vertex" "vs_4_0" "120"
+	Compile "fs_*.sc" "fragment" "ps_4_0" "120"
+    Compile "cs_*.sc" "compute" "cs_5_0" "430"
 }
 
 Write-Output ""
